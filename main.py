dfeaf67d8f317e72b216467b88fce47e4c065f79
sverdlov_ns <nsverdov@bk.ru>
2024-02-05 17:41:54 +0300

    омаем голову над переходом и продавцом вб

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 import csv
 import time
-from struct import Items
+from struct import Items, Item
 
 from playwright.async_api import async_playwright
 from playwright.async_api import TimeoutError
@@ -12,6 +12,7 @@ class WbParser:
     def __init__(self):
         self.page = None
         self.context = None
+        self.items = Items()
 
     async def __page_down(self, page):
         time.sleep(3)
@@ -34,13 +35,16 @@ class WbParser:
         ''')
 
     def __create_csv(self):
-        with open('marketPlaces.csv', mode='w', newline='', encoding='UTF-8') as file:
+        with open('marketPlaces.csv', mode='w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
-            writer.writerow(['Наименование товара', 'Цена', 'Рейтинг', 'Ссылка'])
+            writer.writerow(['Наименование товара', 'Цена', 'Рейтинг', 'Ссылка', 'Продавец', 'Продавец_URL'])
 
     def __save_csv(self):
-        with open('marketPlaces.csv', mode='w', newline='', encoding='UTF-8') as file:
+        with open('marketPlaces.csv', mode='w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
+            writer.writerow(['Наименование товара', 'Цена', 'Рейтинг', 'Ссылка', 'Продавец' 'Продавец_URL'])
+            for product in self.items.products:
+                writer.writerow([product.name, product.price, product.rating, product.url, product.name_of_seller, product.link_on_seller])
 
     async def __get_info(self, url: str):
         self.page2 = await self.context.new_page()
@@ -51,15 +55,34 @@ class WbParser:
             name_of_product = await self.page2.locator(".product-page__title").text_content()
             price_of_product = await self.page2.inner_text(".price-block__wallet-price", TimeoutError=1000)
             rate_of_product = await self.page2.inner_text(".product-review__rating")
-
-            print(name_of_product + " " + price_of_product + " " + rate_of_product)
+            name_of_seller = await self.page2.inner_text(".seller-info__name")
+            url_on_seller = await self.page2.query_selector(".seller-info__name")
+            link_on_seller = await url_on_seller.get_attribute("href")
+            item = Item(url, name_of_product, price_of_product, rate_of_product, name_of_seller, link_on_seller)
+            self.items.products.append(item)
+            self.__save_csv()
+            print(
+                name_of_product + " " + price_of_product + " " + rate_of_product + " " + name_of_seller + " " + link_on_seller)
             await self.page2.close()
         except:
             name_of_product = await self.page2.locator(".product-page__title").text_content()
             price_of_product = await self.page2.inner_text(".price-block__final-price")
             rate_of_product = await self.page2.inner_text(".product-review__rating")
-            print(name_of_product + " " + price_of_product + " " + rate_of_product)
+            name_of_seller = await self.page2.inner_text(".seller-info__name")
+            url_on_seller = await self.page2.query_selector(".seller-info__name")
+            link_on_seller = "https://www.wildberries.ru" + await url_on_seller.get_attribute("href")
+            item = Item(url, name_of_product, price_of_product, rate_of_product, name_of_seller, link_on_seller)
+            self.items.products.append(item)
+            self.__save_csv()
+            print(
+                name_of_product + " " + price_of_product + " " + rate_of_product + " " + name_of_seller + " " + link_on_seller)
             await self.page2.close()
+
+    async def __go_next_page(self):
+        count = 20
+        for i in range(count):
+            await self.page.get_by_text("Следующая страница").click()
+            await self.__page_down(self.page)
 
     async def __get_links(self):
         await self.page.wait_for_selector(".product-card-list")
@@ -68,13 +91,18 @@ class WbParser:
         time.sleep(15)
         search_result = await self.page.query_selector(".product-card-list")
         links = await search_result.query_selector_all(".product-card__link")
+        #await self.__go_next_page()
+        print(len(links))
         for count, link in enumerate(links):
-            if count > 200: break
+            if count > 100000: break
             time.sleep(0.2)
             url = await link.get_attribute("href")
             await self.__get_info(url=url)
 
+
+
     async def parse(self):
+        self.__create_csv()
         async with async_playwright() as p:
             browser = await p.firefox.launch(headless=False)
             self.context = await browser.new_context()
--- struct.py
+++ struct.py
@@ -1,10 +1,13 @@
-
 class Item():
-    name: str
-    price: float
-    rating: float
-    url: str
+    def __init__(self, url: str, name: str, price: float, rating: float, name_of_seller: str, link_on_seller: str):
+        self.url = url
+        self.name = name
+        self.price = price
+        self.rating = rating
+        self.name_of_seller = name_of_seller
+        self.link_on_seller = link_on_seller
 
 
 class Items():
-    products: list[Item]
+    def __init__(self):
+        self.products = []
